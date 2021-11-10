0e9f03b9a7af67bb1f86ac17ab87fd9ce6f1d6de
pubgaceplayer <88487152+pubgaceplayer@users.noreply.github.com>
2021-11-10 15:25:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var student1
+var student2
+function setup() {
+  createCanvas(400,400);
+student1=new Student("mortal",26,9)
+student2=new Student("scout",25,9)
+student1.display();
+student2.display();
+}
+
+function draw() 
+{
+  background(30);
+ 
+}
